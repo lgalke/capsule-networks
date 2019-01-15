acaf3f8d170b4f6d0f34aaefc6ddfad4547171d5
Lukas Galke <git@lpag.de>
2019-01-15 15:15:08 +0100

    Impr graph gen and viz

--- routing.py
+++ routing.py
@@ -1,47 +1,41 @@
 #!/usr/bin/env python3
+import numpy as np
 import torch
+import torch.nn as nn
 from capsule_network import CapsuleLayer
+import networkx as nx
+import matplotlib
+matplotlib.use('tkagg')
 
+import matplotlib.pyplot as plt
 
-"""
-A toy routing example:
-
-Consider dummy city
-with following segments
-
-
--------------
-|   |   |   |
-| A | B | C |
-|   |   |   |
--------------
-|   |   |   |
-| D | E | F |
-|   |   |   |
--------------
-
-with connections between adjacent segments
-
-A -- B
-A -- D
-B -- C
-B -- E
-C -- F
-D -- E
-E -- F
-
-"""
 
 # Number of segments to consider
-N_SEGMENTS = 6
+N_SEGMENTS = 10 
 
 # We sample new loads for each timestep
 N_TIMESTEPS = 1
 
-def generate_data(n_timesteps, n_segments):
+
+def show_city(adjacency):
+    adjacency = np.asarray(adjacency)
+    graph = nx.Graph(adjacency)
+    plt.figure(1)
+    nx.draw(graph)
+    plt.show()
+
+
+def generate_data(n_segments, n_timesteps):
     """ Generate toy data """
+    # Random lobster looks as messy as typical (german) cities
+    graph = nx.random_lobster(10, 0.9, 0.9)
+    adjacency = torch.from_numpy(nx.to_numpy_array(graph))
     # Connections among segments
-    adjacency = torch.rand(n_segments, n_segments) < 0.5
+    # adjacency = torch.rand(n_segments, n_segments) < 0.1
+    # # Make connections symetric
+    # adjacency |= adjacency.t()
+    # # Insert diagonal for self-connections
+    # adjacency |= torch.eye(n_segments).byte()
 
     loads = torch.softmax(torch.rand(n_timesteps, n_segments), dim=1)
     # passengers: [timestep, src, dst]
@@ -54,7 +48,7 @@ def generate_data(n_timesteps, n_segments):
 def main():
     """ Run a toy experiment """
     print("Initializing %d segments randomly" % N_SEGMENTS)
-    adjacency, loads, passengers = generate_data(N_TIMESTEPS, N_SEGMENTS)
+    adjacency, loads, passengers = generate_data(N_SEGMENTS, N_TIMESTEPS)
     print("Load per segments:", loads, loads.size(), sep='\n')
     print("Passengers:", passengers, passengers.size(), sep='\n')
     print("Adjacency:", adjacency, adjacency.size(), sep='\n')
@@ -66,6 +60,11 @@ def main():
     print("Output:", out.squeeze())
     # TODO check how to proceed in other impl
 
+    target = torch.eye(N_SEGMENTS)
+    criterion = nn.MSELoss(passengers, target)
+    print("Initial loss: ", criterion(passengers))
+
+    show_city(adjacency)
 
 if __name__ == '__main__':
     main()
